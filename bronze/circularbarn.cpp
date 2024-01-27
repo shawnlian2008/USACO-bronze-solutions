7b30492ac3f0f3b0f261fbb04bf7e0528645eb73
Shawn L <143904182+shawnlian2008@users.noreply.github.com>
2024-01-27 14:27:54 -0600

    Create circularbarn.cpp

---
+++ bronze/circularbarn.cpp
@@ -0,0 +1,29 @@
+// Source: https://usaco.guide/general/io
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  freopen("cbarn.in", "r", stdin);
+  freopen("cbarn.out", "w", stdout);
+	int n; cin >> n;
+	vector<int> room_capacities(n);
+	for (int i = 0; i < n; i++)
+		cin >> room_capacities[i];
+	
+	int mn = INT_MAX;
+	for (int room_number = 0; room_number < n; room_number++){
+		int counter = 0, curr_room = room_number, sum = 0;
+		while (counter < n){
+			if (curr_room == n){
+				curr_room = 0;
+			}
+			sum += room_capacities[curr_room] * counter;
+			curr_room++;
+			counter++;
+		}
+		mn = min(mn, sum);
+	}
+
+	cout << mn << endl;
+}
