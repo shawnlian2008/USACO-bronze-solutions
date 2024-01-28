c4399a756249c9a2879afd810a1ad7a3f4b2d84c
Shawn L <143904182+shawnlian2008@users.noreply.github.com>
2024-01-28 08:22:15 -0600

    Create do_you_know_your_abcs.cpp

---
+++ bronze/do_you_know_your_abcs.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    vector<int> v(7);
+	for (int i = 0; i < 7; i++)
+		cin >> v[i];
+	sort(v.begin(), v.end());
+	cout << v[0] << " " << v[1] << " " << v[6] - v[0] - v[1] << endl;
+}
