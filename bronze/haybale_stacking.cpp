5f906a21af0154a90a39dcf054251144ca2f7dfe
Shawn L <143904182+shawnlian2008@users.noreply.github.com>
2024-02-11 10:32:51 -0600

    Create haybale_stacking.cpp

---
+++ bronze/haybale_stacking.cpp
@@ -0,0 +1,25 @@
+// Source: https://usaco.guide/general/io
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int n, k; cin >> n >> k;
+	vector<int> v(n + 5, 0);
+	for (int i = 0; i < k; i++){
+		int start, end; cin >> start >> end;
+		v[start]++; v[end + 1]--;
+	}
+
+	int factor = 0;
+	vector<int> stacks(n + 1, 0);
+	for (int i = 1; i <= n; i++){
+		factor += v[i];
+		stacks[i] += factor;
+	}
+
+	stacks.erase(stacks.begin());
+	sort(stacks.begin(), stacks.end());
+
+	cout << stacks[n / 2] << endl;
+}
