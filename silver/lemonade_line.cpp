f680b70e5106be9adfc686d5c3b601e0195123c1
Shawn L <143904182+shawnlian2008@users.noreply.github.com>
2024-03-30 14:03:29 -0500

    Update lemonade_line.cpp

--- silver/lemonade_line.cpp
+++ silver/lemonade_line.cpp
@@ -2,24 +2,19 @@
 using namespace std;
 
 int main() {
-    freopen("lemonade.in", "r", stdin);
-    freopen("lemonade.out", "w", stdout);
+  	freopen("lemonade.in", "r", stdin);
+  	freopen("lemonade.out", "w", stdout);
 	int n; cin >> n;
-	vector<pair<int, int>> w(n);
-	map<int, int> id_to_w;
-	for (int i = 0; i < n; i++){
-		w[i].first = i;
-		cin >> w[i].second;
-		id_to_w[i] = w[i].second;
-	}
-	
-	sort(w.rbegin(), w.rend(), [](const pair<int, int> a, const pair<int, int> b){
-		return a.second < b.second;
-	});
+	vector<int> w(n); // Pair of cows in the format {id, wait}
+	for (int i = 0; i < n; i++)
+		cin >> w[i];
+
+	// Sort by wait time
+	sort(w.rbegin(), w.rend());
 
 	vector<int> line;
 	for (int i = 0; i < n; i++){
-		if (w[i].second < line.size())
+		if (w[i] < line.size())
 			continue;
 		else line.push_back(i);
 	}
