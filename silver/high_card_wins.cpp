42c053548014700a42f76c712eae5d2c249b71f9
Shawn L <143904182+shawnlian2008@users.noreply.github.com>
2024-03-30 12:45:21 -0500

    Update high_card_wins.cpp

--- silver/high_card_wins.cpp
+++ silver/high_card_wins.cpp
@@ -7,8 +7,8 @@ int main() {
 	freopen("highcard.in", "r", stdin);
 	freopen("highcard.out", "w", stdout);
 	int n; cin >> n;
-	vector<int> e(n);
-	map<int, bool> used;
+	vector<int> e(n); // Elsie's cards
+	map<int, bool> used; // Determine which cards Bessie gets from Elsie's cards
 	multiset<int> b;
 	for (int i = 0; i < n; i++){
 		cin >> e[i];
@@ -17,24 +17,19 @@ int main() {
 
 	for (int i = 1; i <= 2 * n; i++)
 		if (!used[i])
-			b.insert(i);
+			b.insert(i); // Bessie gets all the other cards that Elsie doesn't get from 1 to 2 * n
 
-	sort(e.begin(), e.end());
+	sort(e.begin(), e.end()); // Sort Elsie's cards
 
 	int ans = 0;
-	for (int i = 0; i < e.size(); i++){
-		auto it = b.upper_bound(e[i]);
+	for (int i = 0; i < e.size(); i++){ // Go through all of Elsie's cards
+		auto it = b.upper_bound(e[i]); // Lowest card that is larger than Elsie's current card
 
-		/*for (int i : b) cout << i << " ";
-		cout << endl;*/
-
-		//no possible win for bessie this round
-		if (it == b.end()){
-			//use the smallest card in the set
-			b.erase(b.begin());
+		if (it == b.end()){ // No possible win for Bessie this round, because there are no cards larger than Elsie's
+			b.erase(b.begin()); // Use the worst card, or smallest card that Bessie has
 		} else {
 			ans++;
-			b.erase(it);
+			b.erase(it); // Remove the lowest card larger than Elsie's current card
 		}
 	}
 
