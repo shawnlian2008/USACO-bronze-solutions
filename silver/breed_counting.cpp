491ae752da5c6fb472553249b0c7e529efdaba0d
Shawn L <143904182+shawnlian2008@users.noreply.github.com>
2024-03-30 12:32:31 -0500

    Update breed_counting.cpp

--- silver/breed_counting.cpp
+++ silver/breed_counting.cpp
@@ -1,14 +1,14 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main() {
+int main() { // Tags: Prefix sums
 	freopen("bcount.in", "r", stdin);
-  freopen("bcount.out", "w", stdout);
+  	freopen("bcount.out", "w", stdout);
 	int n, q; cin >> n >> q;
 	
-	vector<int> num_1(n + 1), num_2(n + 1), num_3(n + 1);
+	vector<int> num_1(n + 1), num_2(n + 1), num_3(n + 1); // Prefix sums array of all 3 breeds
 
-	for (int i = 1; i <= n; i++){
+	for (int i = 1; i <= n; i++){ // Go through all indices
 		int breed; cin >> breed;
 		num_1[i] = num_1[i - 1];
 		num_2[i] = num_2[i - 1];
@@ -21,13 +21,13 @@ int main() {
 		//cout << num_1[i] << " " << num_2[i] << " " << num_3[i] << endl;
 	}
 
-	for (int i = 0; i < q; i++){
+	for (int i = 0; i < q; i++){ // Each query
 		int l, r; cin >> l >> r;
 
 		int a, b, c;
-		a = num_1[r] - num_1[l - 1];
-		b = num_2[r] - num_2[l - 1];
-		c = num_3[r] - num_3[l - 1];
+		a = num_1[r] - num_1[l - 1]; // Breed 1
+		b = num_2[r] - num_2[l - 1]; // Breed 2
+		c = num_3[r] - num_3[l - 1]; // Breed 3
 
 		cout << a << " " << b << " " << c << endl;
 	}
