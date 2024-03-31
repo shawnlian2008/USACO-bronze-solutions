0d4940dd1823153a9df6302983b9410795bbf7bd
Shawn L <143904182+shawnlian2008@users.noreply.github.com>
2024-03-31 15:10:37 -0500

    Update subsequences_summing_to_seven.cpp

--- silver/subsequences_summing_to_seven.cpp
+++ silver/subsequences_summing_to_seven.cpp
@@ -2,22 +2,21 @@
 typedef long long ll;
 using namespace std;
 
-int main() {
+int main() { // Tags: Prefix sums
     freopen("div7.in", "r", stdin);
     freopen("div7.out", "w", stdout);
 
     //Take inputs by their sum mod 7
-    int n;
-    cin >> n;
+    int n; cin >> n;
     vector<int> cows(n+1);
     for (int i = 1; i <= n; i++){
         cin >> cows[i];
         cows[i] = (cows[i]+cows[i-1])%7;
     }
 
-    //Find the first and last of the same value in the list
-    //The cow ids between them must be divisible by 7
-    //The difference between the first and last of these index must be the answer
+    // Find the first and last of the same value in the list
+    // The cow ids between them must be divisible by 7
+    // The difference between the first and last of these index must be the answer
     int ans = 0;
     vector<int> left(7);
     vector<int> right(7);
