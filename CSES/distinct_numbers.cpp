004b73907122a2c131d4be4f428f6867d1dbd107
Shawn L <143904182+shawnlian2008@users.noreply.github.com>
2024-04-06 20:12:02 -0500

    Update distinct_numbers.cpp

--- CSES/distinct_numbers.cpp
+++ CSES/distinct_numbers.cpp
@@ -3,7 +3,7 @@ using namespace std;
  
 int main(void){
     int n; cin >> n;
-    set<int> s;
+    set<int> s; // Sets hold distinct numbers
     for (int i = 0; i < n; i++){
         int a; cin >> a;
         s.insert(a);
