f42e9e1ca18ef0c19dc35a4ee8c7630db0ec801f
Shawn L <143904182+shawnlian2008@users.noreply.github.com>
2024-04-06 20:23:54 -0500

    Update repetitions.cpp

--- CSES/repetitions.cpp
+++ CSES/repetitions.cpp
@@ -4,15 +4,15 @@ using namespace std;
 int main() {
     string s; cin >> s;
  
-    int longest_perm = 1, perm = 1;
-    for (int i = 1; i < s.length(); i++){
-        if (s[i] != s[i - 1]){
-            longest_perm = max(longest_perm, perm);
+    int longest_perm = 1, perm = 1; // Instance Variables
+    for (int i = 1; i < s.length(); i++){ // Go through string
+        if (s[i] != s[i - 1]){ // Reset
+            longest_perm = max(longest_perm, perm); // Running maximum
             perm = 1;
-        } else perm++;
+        } else perm++; // Add to the longest permutation
     }
  
-    longest_perm = max(longest_perm, perm);
+    longest_perm = max(longest_perm, perm); // Last sequence of the string
  
     cout << longest_perm << endl;
 }
