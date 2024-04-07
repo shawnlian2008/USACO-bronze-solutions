061f1ad08a75d15e46d1a48b85340b4210fab735
Shawn L <143904182+shawnlian2008@users.noreply.github.com>
2024-04-06 20:11:49 -0500

    Create distinct_numbers.cpp

---
+++ CSES/distinct_numbers.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main(void){
+    int n; cin >> n;
+    set<int> s;
+    for (int i = 0; i < n; i++){
+        int a; cin >> a;
+        s.insert(a);
+    }
+ 
+    cout << s.size() << endl;
+}
