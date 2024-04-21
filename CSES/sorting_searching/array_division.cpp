1b3a7e320f3735c6c3ed4d0094fb190cc4323f68
Shawn L <143904182+shawnlian2008@users.noreply.github.com>
2024-04-20 20:17:25 -0500

    Update array_division.cpp

--- CSES/sorting_searching/array_division.cpp
+++ CSES/sorting_searching/array_division.cpp
@@ -1,10 +1,10 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main() {
+int main() { // Tags: Binary Search
 	int n, k; cin >> n >> k;
 	vector<int> numbers(n);
-	int largest_value = INT_MIN;
+	int largest_value = INT_MIN; // Answer has to be higher than largest value, so we will record that and then set the low for Binary Search
 	for (int i = 0; i < n; i++){
 		cin >> numbers[i];
 		largest_value = max(largest_value, numbers[i]);
@@ -12,20 +12,21 @@ int main() {
 
 	long long high = 1e18, low = largest_value, ans = 1e18;
 
-	while (low <= high){
-		long long mid = (low + high) / 2, sum = 0;
-		int split_counter = 1;
-		for (int i = 0; i < n; i++){
-			if (sum + numbers[i] > mid){
-				sum = 0;
+	while (low <= high){// Normal while loop condition for binary search
+		long long mid = (low + high) / 2, sum = 0; // Calculate middle value in between low nad high
+		int split_counter = 1; // Track the number of splits we have to make in order to fit every subarray sum to a value lower than the current value (mid) that we are testing
+		for (int i = 0; i < n; i++){ // Go through all numbers
+			if (sum + numbers[i] > mid){ // Cannot fit anymore
+				sum = 0; // Reset sum and split again
 				split_counter++;
 			}
-			sum += numbers[i];
+			sum += numbers[i]; // Add to sum
 		}
 
-		if (split_counter > k)
+		if (split_counter > k) // Too many splits
 			low = mid + 1;
-		else {
+		else { // Valid number of splits, keep going
+			// Find the minimum valid largest value with binary search
 			ans = min(ans, mid); 
 			high = mid - 1;
 		}
