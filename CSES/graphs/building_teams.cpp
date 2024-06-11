3464f2890942e22ade28943b85dc66e236448bf6
Shawn L <143904182+shawnlian2008@users.noreply.github.com>
2024-06-10 22:32:16 -0500

    Update building_teams.cpp

--- CSES/graphs/building_teams.cpp
+++ CSES/graphs/building_teams.cpp
@@ -1,22 +1,22 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-vector<vector<int>> friends;
-vector<bool> visited;
-vector<int> assigned;
+vector<vector<int>> friends; // Holds the graph of friend links
+vector<bool> visited; // If each node has been visited or not
+vector<int> assigned; // Assign team numbers to each person after team division
 
-bool yuh;
+bool yuh; // If it is impossible or not to assign these 2 teams
 void dfs(int node, int assignment){
-	//cout << assignment << endl;
-	assigned[node] = assignment;
-	visited[node] = true;
+	assigned[node] = assignment; // Assign team number
+	visited[node] = true; // Mark as visited
 	for (int i = 0; i < friends[node].size(); i++){
-		if (assigned[friends[node][i]] == assignment){
+		if (assigned[friends[node][i]] == assignment){ // If there already exists a neighbour with the supposed assignment for this current node
 			yuh = true;
 			return;
 		}
 
-		if (!visited[friends[node][i]]){
+		if (!visited[friends[node][i]]){ // If the node hasn't been visited
+			// Recurse dfs and change assignment
 			if (assignment == 1) dfs(friends[node][i], 2);
 			else if (assignment == 2) dfs(friends[node][i], 1);
 		}
@@ -38,14 +38,15 @@ int main() {
 	}
 
 	int dist = 1;
-	for (int i = 0; i < n; i++){
+	for (int i = 0; i < n; i++){ // DFS each set of connected components
 		if (visited[i]) continue;
 	
 		dfs(i, 1);
 	}
 
+	// Output
 	if (yuh) cout << "IMPOSSIBLE" << endl;
-	else {
+	else { 
 		for (int i = 0; i < n; i++){
 			cout << assigned[i];
 			if (i != n - 1) cout << " ";
